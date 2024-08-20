886bc248aab8b96c8f27035d3c2434e726fd3373
aquib-11 <135202503+aquib-11@users.noreply.github.com>
2024-08-20 14:45:58 +0530

    basic structure

---
+++ client/src/components/index.js
@@ -0,0 +1,2 @@
+export { default as Header } from "./sharedComponents/Header";
+export { default as Footer } from "./sharedComponents/Footer";
---
+++ client/src/layouts/HomeLayout.jsx
@@ -0,0 +1,13 @@
+import { Outlet } from "react-router-dom"
+import { Wrapper } from "../assets/styles/HomeLayout"
+import { Footer, Header } from "../components"
+const HomeLayout = () => {
+  return (
+    <Wrapper>
+        <Header/>
+        <Outlet/>
+        <Footer/>
+    </Wrapper>
+  )
+}
+export default HomeLayout
