42d5d369af3d34ebee73f4783820f22d5fae1166
SHAHID AJAZ <121556407+Shahid0019@users.noreply.github.com>
2024-08-21 14:35:07 +0530

    changes

--- client/src/components/index.js
+++ client/src/components/index.js
@@ -3,3 +3,4 @@ export { default as Footer } from "./sharedComponents/Footer";
 export { default as Hero } from "./homeComponents/Hero";
 export { default as TodayEssemtials } from "./homeComponents/TodayEssentials";
 export { default as Stastistics } from "./homeComponents/Stastistics";
+export { default as Podcasts } from "./homeComponents/Podcasts";
