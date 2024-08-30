5c9656c7fd9ff4fb46d144324343421abefba075
aquib-11 <135202503+aquib-11@users.noreply.github.com>
2024-08-30 13:15:00 +0530

    complete articles page

--- client/src/pages/articles/Articles.jsx
+++ client/src/pages/articles/Articles.jsx
@@ -1,26 +1,90 @@
 import { articles } from "@/utils/Articles";
 import { Article, Search, SimilarArticles } from "@/components";
+import { useLoaderData } from "react-router-dom";
+import React, { useState } from "react";
+import { data } from "autoprefixer";
+import notFoundBySearch from "../../assets/images/notFoundBySearch.jpg";
+
+export const ArticlesLoader = async ({ request }) => {
+  const url = new URL(request.url);
+  const searchParams = Object.fromEntries(url.searchParams.entries());
+
+  const {
+    search = "",
+    category: selectedCategory = "",
+    sort_by: sortBy = "",
+  } = searchParams;
+
+
+  let filteredArticles = [...articles]
+
+  if (search) {
+    filteredArticles = filteredArticles.filter((article) =>
+      article.title.toLowerCase().includes(search.toLowerCase())
+    );
+  }
+
+  if (selectedCategory) {
+    filteredArticles = filteredArticles.filter((article) =>
+      article.tags.includes(selectedCategory)
+    );
+  }
+
+  if (sortBy === "newest") {
+    filteredArticles.sort(
+      (a, b) => new Date(b.datePublished) - new Date(a.datePublished)
+    );
+  } else if (sortBy === "oldest") {
+    filteredArticles.sort(
+      (a, b) => new Date(a.datePublished) - new Date(b.datePublished)
+    );
+  } else if (sortBy === "most viewed") {
+    filteredArticles.sort((a, b) => b.views - a.views);
+  }
+
+  console.log("Final filtered articles:", filteredArticles); // Log final filtered articles
+
+  const allTags = articles.flatMap((article) => article.tags);
+  const category = Array.from(new Set(allTags));
+
+  return { data: filteredArticles, category };
+};
+
 
 const Articles = () => {
+  const {data} = useLoaderData()
   return (
-    <div className="max-w-7xl mx-auto px-4 bg-white mt-9">
-      {/* aricles */}
-      <div className="grid grid-cols-12 gap-4">
-        <div className="col-span-8 grid gap-6  p-4 rounded">
-          {articles.map((article, index) => {
-            return <Article key={index} article={article} />;
-          })}
+    <div className="max-w-7xl mx-auto p-4 bg-white mt-20 ">
+      <div className=" grid grid-cols-1 lg:grid-cols-3 lg:custom-grid-row lg:gap-x-8 lg:gap-y-4">
+        {/* aricles */}
+        <div className="grid gap-6 rounded order-2 lg:col-span-2 lg:row-span-2 lg:order-1">
+          {data.length > 0 ? (
+            data.map((article, index) => {
+              return <Article key={index} article={article} />;
+            })
+          ) : (
+            <p className="pt-4 pl-4">
+              no article found ! Please try searching for different keywords or adjusting your
+              filters.
+              <img className="lg:h-[70vh] md:h-[50vh] h-[200px]" src={notFoundBySearch}  alt="not found"/>
+            </p>
+          )}
         </div>
-        {/* sidebar */}
+        {/* search filters*/}
         <div
-          className="col-span-4 bg-white shadow-xl p-4 rounded flex flex-col gap-8 sticky top-4"
+          className="bg-white shadow-xl p-4 rounded order-1 lg:sticky lg:top-[5rem] lg:z-10"
           style={{ height: "max-content" }}
         >
-          {/* search filters*/}
           <Search />
+        </div>
+        {/* more articles */}
+        <div
+          className="bg-white shadow-xl p-4 rounded flex flex-col gap-8 order-3"
+          style={{ height: "max-content" }}
+        >
           {/* similar articles*/}
           <SimilarArticles title="Recent Articles" />
-          {/* recent articles*/}
+          {/* recnt articles*/}
           <SimilarArticles title="Similar Articles" />
         </div>
       </div>
--- client/src/pages/index.js
+++ client/src/pages/index.js
@@ -1,3 +1,12 @@
 export { default as About } from "./About";
 export { default as Home } from "./Home";
 export { default as Articles } from "./articles/Articles";
+
+
+
+
+
+// loader
+
+
+// actions
