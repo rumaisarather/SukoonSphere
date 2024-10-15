505320ec6163af62e0056b3d8f9cef4002fd8f6f
SHAHID AJAZ <121556407+Shahid0019@users.noreply.github.com>
2024-10-15 20:20:29 +0530

    reCreated the Nav bar

--- client/src/components/homeComponents/HeaderComponents/NavMenu.jsx
+++ client/src/components/homeComponents/HeaderComponents/NavMenu.jsx
@@ -1,80 +1,72 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import LinkButton from "@/components/sharedComponents/Buttons/LinkButton";
 import CompanyLogo from "../../../assets/images/SukoonSphere_Logo.png";
-import { links } from "../../../utils/SharedComp/PageLinks";
+import { links } from "@/utils/SharedComp/PageLinks";
 import { Link, NavLink } from "react-router-dom";
 import { GiHamburgerMenu } from "react-icons/gi";
 import { RxCross2 } from "react-icons/rx";
 import { useAuth0 } from "@auth0/auth0-react";
 import { BsThreeDotsVertical } from "react-icons/bs";
+import { FaArrowRightLong } from "react-icons/fa6";
+import { BsFillTriangleFill } from "react-icons/bs";
 
 function NavMenu() {
-  const [isSticky, setIsSticky] = useState(false);
   const [menuOpen, setMenuOpen] = useState(false);
   const [miniMenu, setMiniMenu] = useState(false);
 
-  const { isAuthenticated, loginWithRedirect, isLoading, user, logout } =
-    useAuth0();
+  const { isAuthenticated, loginWithRedirect, user, logout } = useAuth0();
   const isUser = isAuthenticated && user;
 
   const toggleMenu = () => {
     setMenuOpen(!menuOpen);
   };
+
   const toggleMiniMenu = () => {
     setMiniMenu(!miniMenu);
   };
-  useEffect(() => {
-    const handleScroll = () => {
-      setIsSticky(window.scrollY > 47);
-    };
 
-    window.addEventListener("scroll", handleScroll);
-    return () => {
-      window.removeEventListener("scroll", handleScroll);
-    };
-  }, []);
   return (
-    <nav
-      className={` flex items-center sm:items-center justify-between sm:flex-row  w-full z-20 transition-all ease-in-out p-2 h-[60px]  ${isSticky
-        ? "fixed top-0 duration-500 bg-gradient-to-r from-[var(--primary)] to-[var(--secondary)] text-white shadow-md"
-        : "sticky shadow-md"
-        }`}
-    >
+    <nav className="flex bg-white sticky top-0 items-center justify-between shadow-[0px_1px_10px_rgba(0,0,0,0.1)] w-full z-20 transition-all ease-in-out p-2 h-[60px]">
       <GiHamburgerMenu
-        className={`block place-content-end absolute right-3 md:hidden cursor-pointer text-[1.4rem]  ${isSticky ? "text-white" : "text-[var(--secondary)]"} `}
+        className="block absolute right-3 md:hidden cursor-pointer text-[1.4rem]"
         onClick={toggleMenu}
       />
-      <div className="flex w-full justify-between items-center  px-28  ">
+      <div className="flex w-full justify-between items-center px-28">
         <img
           src={CompanyLogo}
-          className="object-contain w-14 "
+          className="object-contain w-14"
           alt="Logo Loading..."
         />
-        <div className="hidden md:block">
-          <ul className="flex gap-5 sm:justify-between dsm:items-center">
+        <div className="hidden md:flex justify-center flex-grow">
+          <ul className="flex mt-10 h-16 gap-5 items-center">
             {links.map((link) => (
-              <div className="relative group" key={link.name}>
-                <NavLink
-                  to={link.address}
-                  className={({ isActive }) =>
-                    `font-extrabold ${isActive
-                      ? "text-[var(--ternery)]"
-                      : `${isSticky ? "text-white" : "text-[var(--secondary)]"}`
-                    } hover:text-[var(--ternery)] transition-all duration-300`
-                  }
-                >
-                  {link.name}
+              <div
+                className="group relative h-full align-middle"
+                key={link.name}
+              >
+                <NavLink to={link.address} className="flex items-center">
+                  {link.icon}
+                  <span className="ml-1">{link.name}</span>
                 </NavLink>
 
-                {link.name === "About" && link.sublinks && (
-                  <ul className="absolute hidden group-hover:block bg-white shadow-lg p-2 rounded-md w-48">
+                {link.sublinks && (
+                  <ul className="top-14 absolute opacity-0 invisible group-hover:opacity-100 group-hover:visible transition-opacity duration-200 ease-in-out transform group-hover:translate-y-1 shadow-[0px_1px_10px_rgba(0,0,0,0.1)] bg-white p-2 rounded-[5px] w-48">
+                    <BsFillTriangleFill className="text-lg absolute -top-3 left-5 text-white" />
+
                     {link.sublinks.map((sublink) => (
-                      <li key={sublink.name} >
+                      <li
+                        key={sublink.name}
+                        className="transition-opacity duration-300 ease-in-out"
+                      >
                         <NavLink
                           to={sublink.address}
-                          className="block px-4 py-2 text-[var(--primary)] hover:text-[var(--ternery)] transition-all duration-200"
+                          className="flex items-center gap-2 px-2 py-2 text-[var(--primary)] hover:text-[var(--ternery)] opacity-0 group-hover:opacity-100 transition-opacity duration-200"
                         >
-                          {sublink.name}
+                          {" "}
+                          <div className="bg-[var(--primary)] rounded-full p-8">
+                            {sublink.icon}
+                          </div>
+                          <span>{sublink.name}</span>
                         </NavLink>
                       </li>
                     ))}
@@ -93,22 +85,19 @@ function NavMenu() {
                 alt="User"
               />
               <BsThreeDotsVertical
-                className={`block cursor-pointer  text-[1.4rem] ${isSticky ? 'text-white' : 'text-[var(--secondary)]'}`}
+                className="block cursor-pointer text-[1.4rem]"
                 onClick={toggleMiniMenu}
               />
-              <Link to={'about/user'}>
-                <span className="text-black ">{user.name}</span>
+              <Link to={"about/user"}>
+                <span className="text-black">{user.name}</span>
               </Link>
             </div>
             <div
-              className={`
-                ${miniMenu
-                  ? "opacity-100 max-h-[500px]"
-                  : "opacity-0 max-h-0"} 
-                 absolute overflow-hidden transition-all duration-300 ease-in-out p-2 shadow-lg rounded-[4px] bg-white flex flex-col w-44 top-[4.5rem] right-[7.5rem]`}
+              className={`${
+                miniMenu ? "opacity-100 max-h-[500px]" : "opacity-0 max-h-0"
+              } absolute overflow-hidden transition-all duration-300 ease-in-out p-2 shadow-lg rounded-[4px] bg-white flex flex-col w-44 top-[4.5rem] right-[7.5rem]`}
               style={{
-                transition:
-                  "opacity 0.5s ease, max-height 0.5s ease ",
+                transition: "opacity 0.5s ease, max-height 0.5s ease ",
               }}
             >
               <div className="flex items-center gap-3 pb-3">
@@ -117,36 +106,47 @@ function NavMenu() {
                   src={user.picture}
                   alt="User"
                 />
-                <Link to={"about/user"}> <span className="text-black">{user.name}</span> </Link>
+                <Link to={"about/user"}>
+                  <span className="text-black">{user.name}</span>
+                </Link>
               </div>
 
               <button
-                to="/#"
                 onClick={async () => {
                   logout();
                 }}
-                className={`transition-all ease-in-out duration-200  bg-none px-4 py-1 rounded-[4px]  hover:text-white
-        ${isSticky ? " border-[1.5px] border-[var(--secondary)] text-[var(--secondary)] hover:border-[var(--secondary)] hover:bg-[var(--secondary)]" : "border-[1.5px] text-[var(--secondary)]   border-[var(--secondary)] hover:bg-[var(--secondary)]"}`}
+                className={`transition-all ease-in-out duration-200 bg-none px-4 py-1 rounded-[4px] hover:text-white`}
               >
                 Logout
               </button>
             </div>
           </>
         ) : (
-          <button
-            onClick={async () => {
-              loginWithRedirect();
-            }}
-            className={`transition-all ease-in-out duration-200  bg-none px-4 py-1 rounded-[4px]  hover:text-white
-              ${isSticky ? " border-[1.5px] border-white hover:border-[#62cb9c8d] hover:bg-[#62cb9c8d]" : "border-[1.5px] text-[var(--secondary)]  border-[var(--secondary)] hover:bg-[var(--secondary)]"}`}
-          >
-            Login
-          </button>
+          <div className="flex gap-2">
+            <button
+              onClick={async () => {
+                loginWithRedirect();
+              }}
+              className="hidden sm:flex bg-white items-center gap-1 rounded-[5px] shadow-[0_2px_0_0_rgba(0,0,0,0.04),_inset_0_0_0_2px_#e0e0e0] transition-all ease-in-out duration-600 text-gray-900 px-3 py-2 text-xs leading-[1.32] hover:bg-[#f2f6ff]"
+            >
+              <span>Sign in</span>
+              <FaArrowRightLong />
+            </button>
+            <button
+              onClick={async () => {
+                loginWithRedirect();
+              }}
+              className="hidden sm:flex bg-black items-center gap-1 rounded-[5px] transition-all ease-in-out duration-600 text-white px-3 py-2 text-xs leading-[1.32] hover:bg-[#f2f6ff]"
+            >
+              <span>Sign up</span>
+              <FaArrowRightLong />
+            </button>
+          </div>
         )}
       </div>
       <div className="block md:hidden">
         <ul
-          className={`flex flex-col w-80 bg-gradient-to-r from-[var(--primary)] to-[var(--secondary)]  p-4 transition-all  ease-in-out duration-600 absolute top-0 h-[100vh] ${menuOpen ? "left-0" : "-left-full"}`}
+          className={`flex flex-col w-80 bg-gradient-to-r from-[var(--primary)] to-[var(--secondary)] p-4 transition-all ease-in-out duration-600 absolute top-0 h-[100vh] ${menuOpen ? "left-0" : "-left-full"}`}
         >
           <RxCross2
             onClick={toggleMenu}
@@ -159,25 +159,21 @@ function NavMenu() {
               className={({ isActive }) =>
                 isActive
                   ? "text-orange-300 font-bold"
-                  : "text-white  hover:text-orange-300"
+                  : "text-white hover:text-orange-300"
               }
               onClick={toggleMenu}
             >
-              {link.name}
+              {link.icon}
+              <span className="ml-1">{link.name}</span>
             </NavLink>
           ))}
-          <LinkButton
-            to="/#"
-            variant="primary"
-            size="small"
-            className={`
-          ${isSticky && "bg-[var(--btn-primary)]"}`}
-          >
+          <LinkButton to="/#" variant="primary" size="small">
             Login
           </LinkButton>
         </ul>
       </div>
-    </nav >
+    </nav>
   );
 }
+
 export default NavMenu;
