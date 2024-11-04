52c9add236ac4c9cbad212f57ea8e297031b6e3f
aquib-11 <135202503+aquib-11@users.noreply.github.com>
2024-11-04 12:23:41 +0530

    post likes created

--- controllers/postController.js
+++ controllers/postController.js
@@ -2,6 +2,7 @@ import { StatusCodes } from "http-status-codes";
 import Post from "../models/postModel.js";
 import { formatImage } from "../middleware/multer.js";
 import cloudinary from "cloudinary";
+import { BadRequestError } from "../errors/customErors.js";
 export const createPost = async (req, res) => {
   const newPost = {
     createdBy: req.user.userId,
@@ -19,6 +20,31 @@ export const createPost = async (req, res) => {
   res.status(StatusCodes.CREATED).json({ msg: "Post uploaded sucessesfully" });
 };
 export const getAllPosts = async (req, res) => {
-  const posts = await Post.find({})
-    res.status(StatusCodes.OK).json({ posts });
+  const posts = await Post.find({});
+    const postsWithLikes = posts.map((post) => ({
+      ...post.toObject(),
+      totalLikes:post.likes ? post.likes.length : 0, 
+    }));
+  res.status(StatusCodes.OK).json({ postsWithLikes });
+};
+
+export const likePosts = async (req, res) => {
+  const userId = req.user.userId;
+  const postId = req.params.id;
+
+  const post = await Post.findById(postId);
+  if (!post) {
+    throw new BadRequestError("Post not found");
+  }
+
+  if (post.likes.includes(userId)) {
+    post.likes = post.likes.filter((id) => id.toString() !== userId.toString());
+    await post.save();
+    return res.status(StatusCodes.OK).json({ message: "Post unliked successfully", post });
+  } else {
+    post.likes.push(userId);
+    await post.save();
+    return res.status(StatusCodes.OK).json({ message: "Post liked successfully", post });
+  }
+
 };
