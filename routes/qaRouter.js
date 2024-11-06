3a14112b5a2d75726e9bfcf97eacf800a52171b7
aquib-11 <135202503+aquib-11@users.noreply.github.com>
2024-11-06 12:08:35 +0530

    get all questions

--- routes/qaRouter.js
+++ routes/qaRouter.js
@@ -1,9 +1,10 @@
 import { Router } from "express";
 import { authenticateUser } from "../middleware/authMiddleware.js";
-import { addQuestion } from "../controllers/qaController.js";
+import { addQuestion, getAllQuestions } from "../controllers/qaController.js";
 import { validateQaSectionInput } from "../middleware/validationMiddleware.js";
 const router = Router();
 
 router.post("/", authenticateUser, validateQaSectionInput, addQuestion);
+router.get("/",  getAllQuestions);
 
 export default router;
